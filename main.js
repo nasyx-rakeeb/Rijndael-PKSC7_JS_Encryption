f2093c57dd3fec29fa4d106f3d23a04f4f47d107
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-23 12:11:07 +0530

    change setup

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import Rijndael from 'rijndael-js'
+import RijndaelManaged from 'rijndael-js'
 import padder from 'pkcs7-padding'
 import crypto from 'crypto'
 
 let plainText, padded, key, iv, cipher, encrypted
 
-const encryptor = () => {
+const encryptor = async () => {
     plainText = Buffer.from('Here is my plain text', 'utf8')
     padded = padder.pad(plainText, 32)
     key = crypto.randomBytes(32)
     iv = crypto.randomBytes(32)
-    cipher = new Rijndael(key, 'cbc')
+    cipher = new RijndaelManaged(key, 'cbc')
     encrypted = cipher.encrypt(padded, 256, iv)
     console.log(encrypted)
 }
@@ -19,7 +19,7 @@ const decryptor = () => {
     const encrypted = encrypted
     const key = key
     const iv = iv
-    const decipher = new Rijndael(key, 'cbc')
+    const decipher = new RijndaelManaged(key, 'cbc')
     const decryptedPadded = decipher.decrypt(encrypted, 256, iv)
     const decrypted = padder.unpad(decryptedPadded, 32)
     const clearText = decrypted.toString('utf8')
