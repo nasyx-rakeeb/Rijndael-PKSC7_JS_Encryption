13d1a1a5d50683fe037d0c5b09e971d69c6b6bb1
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-23 14:51:55 +0530

    fix bug

--- main.js
+++ main.js
@@ -2,27 +2,27 @@ import RijndaelManaged from 'rijndael-js'
 import padder from 'pkcs7-padding'
 import crypto from 'crypto'
 
-let plainText, padded, key, iv, cipher, encrypted
+let plainText, padded, keyData, ivData, cipher, encryptedData
 
 const encryptor = async () => {
     plainText = Buffer.from('Here is my plain text', 'utf8')
     padded = padder.pad(plainText, 32)
-    key = crypto.randomBytes(32)
-    iv = crypto.randomBytes(32)
-    cipher = new RijndaelManaged(key, 'cbc')
-    encrypted = cipher.encrypt(padded, 256, iv)
-    console.log(encrypted)
+    keyData = crypto.randomBytes(32)
+    ivData = crypto.randomBytes(32)
+    cipher = new RijndaelManaged(keyData, 'cbc')
+    encryptedData = cipher.encrypt(padded, 256, ivData)
+    console.log(encryptedData)
 }
 encryptor()
 
 const decryptor = () => {
-    const encrypted = encrypted
-    const key = key
-    const iv = iv
+    const key = keyData
+    const iv = ivData
+    const encrypted = encryptedData
     const decipher = new RijndaelManaged(key, 'cbc')
     const decryptedPadded = decipher.decrypt(encrypted, 256, iv)
     const decrypted = padder.unpad(decryptedPadded, 32)
-    const clearText = decrypted.toString('utf8')
+    const clearText = Buffer.from(decrypted).toString()
     console.log(clearText)
 }
 decryptor()
