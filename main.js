c76ff9adf5594d67802359f3e34d0b69c46cfc13
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-27 23:55:47 +0530

    add new enc

--- main.js
+++ main.js
@@ -5,13 +5,71 @@ import crypto from 'crypto'
 let plainText, padded, keyData, ivData, cipher, encryptedData
 
 const encryptor = async () => {
-    plainText = Buffer.from('Here is my plain text', 'utf8')
+    plainText = Buffer.from(`{
+    "DPId": "84200",
+    "ReqType": "D",
+    "ReqIdentifier": "S",
+    "ReturnURL": "https://edisapp.azurewebsites.net/edis-callback?dpId=84200",
+    "ReqTime": "21102022110036",
+    "BOID": "1208420000648964",
+    "CMID": "M52003",
+    "ExID": 12,
+    "NumOfDays": 1,
+    "RecordDtls": [{
+            "TxnReqId": "16663500365830",
+            "ISIN": "INF204K01UN9",
+            "Quantity": "0.101"
+        },
+        {
+            "TxnReqId": "16663500365831",
+            "ISIN": "INE589A01014",
+            "Quantity": "3.000"
+        }
+    ],
+    "SettleId": "",
+    "ExecDate": "21102022",
+    "TMID": "",
+    "CtrBOID": "",
+    "TrRsnCode": "",
+    "Filler1": "0",
+    "Filler2": "0"
+}
+
+{
+    "DPId": "84200",
+    "ReqType": "D",
+    "ReqIdentifier": "S",
+    "ReturnURL": "https://edisapp.azurewebsites.net/edis-callback?dpId=84200",
+    "ReqTime": "21102022110036",
+    "BOID": "1208420000648964",
+    "CMID": "M52003",
+    "ExID": 12,
+    "NumOfDays": 1,
+    "RecordDtls": [{
+            "TxnReqId": "16663500365830",
+            "ISIN": "INF204K01UN9",
+            "Quantity": "0.101"
+        },
+        {
+            "TxnReqId": "16663500365831",
+            "ISIN": "INE589A01014",
+            "Quantity": "3.000"
+        }
+    ],
+    "SettleId": "",
+    "ExecDate": "21102022",
+    "TMID": "",
+    "CtrBOID": "",
+    "TrRsnCode": "",
+    "Filler1": "0",
+    "Filler2": "0"
+}`, 'utf8')
     padded = padder.pad(plainText, 32)
-    keyData = crypto.randomBytes(32)
-    ivData = crypto.randomBytes(32)
+    keyData = "lfzv333td7iyq7eafyaem7ecxsse86de"
+    ivData = "lfzv333td7iyq7eafyaem7ecxsse86de"
     cipher = new RijndaelManaged(keyData, 'cbc')
     encryptedData = cipher.encrypt(padded, 256, ivData)
-    console.log(encryptedData)
+    console.log(Buffer.from(encryptedData).toString("hex"))
 }
 encryptor()
 
